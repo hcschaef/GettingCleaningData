3b65618bbd6b42f8518ee626661b12dc3d7762cb
hcschaef <hcschaef@gmail.com>
2014-10-26 20:09:18 +0100

    Bug Fixing
    Bug Fixing in run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@ run_analysis <- function() {
   
   ## Start with Training Data
     ## Load the Activity Labels
-  labels <- read.table("./activity_labels.txt")
+  labels <- read.table("./activity_labels.txt", check.names = FALSE)
   colnames(labels) <- c("Activity_ID","Activity")
     ## Load the Activity IDs for the Training Data
   y_train <- read.table("./train/y_train.txt")
@@ -13,7 +13,7 @@ run_analysis <- function() {
   y_train <- merge(y_train,labels,by="Activity_ID")
   
     ## Load the Feature Lables
-  features <- read.table("./features.txt")
+  features <- read.table("./features.txt", check.names = FALSE)
   colnames(features) <- c("Feature_ID","Feature")
     ## Load the Data Sets for the Training Data
   x_train <- read.table("./train/x_train.txt")
@@ -48,21 +48,21 @@ run_analysis <- function() {
   colnames(subject_test) <- c("Subject_ID", "Type")  
   
   message("Finish Part 1B - Loading Test")
-  message("Start Part 1c - Merging")
+  message("Start Part 1C - Merging")
   
     ## Merging the Training Data in one Data Frame
-  df_train = data.frame(subject_train, y_train, x_train)
-  ##write.table(df_train, "df_train.csv", sep=";", row.name=FALSE)
+  df_train = data.frame(subject_train, y_train, x_train, check.names = FALSE)
+  write.table(df_train, "./CSV_exports/df_train.csv", sep=";", row.name=FALSE)
   
     ## Merging the Test Data in one Data Frame
-  df_test = data.frame(subject_test, y_test, x_test)
-  ##write.table(df_test, "df_test.csv", sep=";", row.name=FALSE)
+  df_test = data.frame(subject_test, y_test, x_test, check.names = FALSE)
+  write.table(df_test, "./CSV_exports/df_test.csv", sep=";", row.name=FALSE)
   
     ## Merging both Data Frames into one
   df <- rbind(df_train, df_test)
-  ##write.table(df, "df_all.csv", sep=";", row.name=FALSE)
+  write.table(df, "./CSV_exports/df_all.csv", sep=";", row.name=FALSE)
   
-  message("Finish Part 1c - Merging")
+  message("Finish Part 1C - Merging")
   message("Start Part 2 - Extrating Mean and Std")
   
   ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
@@ -71,17 +71,23 @@ run_analysis <- function() {
   
     ## Create new Data Frame based on the New Column Names
   df <- df[new_columns]
-  ##write.table(df, "df_mean_std.csv", sep=";", row.name=FALSE)
+  write.table(df, "./CSV_exports/df_mean_std.csv", sep=";", row.name=FALSE)
   
   message("Finish Part 2 - Extrating Mean and Std")
   message("Start Part 3 - Creating the Average")
   
   ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-  
+    ## Get all Column Names of the mean and std
   new_columns2 <- grep("mean|std",names(df),value=TRUE)
+    ## Aggregate the Mean of all mean and std columns based on the column names
   df2 <- aggregate(df[new_columns2], by=df[c("Subject_ID","Activity")], FUN=mean)
-  ##write.table(df2, "df_average.csv", sep=";", row.name=FALSE)
-  write.table(df2, "df_tidy_data.txt", sep=";", row.name=FALSE)
+  write.table(df2, "./CSV_exports/df_average.csv", sep=";", row.name=FALSE)
   
   message("Finish Part 3 - Creating the Average")
+  message("Start Part 4 - Exporting the Tidy Data Set")
+  
+    ## Export the Tidy Data Set
+  write.table(df2, "df_tidy_data.txt", sep=";", row.name=FALSE)
+  
+  message("Finish Part 4 - Exporting the Tidy Data Set")
 }
